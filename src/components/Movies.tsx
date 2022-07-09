9ea6f057d390e6a7496be7487b9350540be79d14
lvneto <lauro.neto@irriga.net>
2022-07-09 13:03:31 -0300

    change layout

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -101,97 +101,105 @@ const customStyles = {
 };
 
   return (
-    <>  
-      <div className="py-2 bg-blue-700 flex items-center sm:px-6 dark:bg-blue-700">        
-        <div className="sm:flex-1 sm:flex justify-center">       
-          <div>     
-            <button className="px-2 mr-2 bg-gray-700 text-white text-sm
-                leading-tight uppercase rounded shadow-md hover:bg-gray-600 hover:shadow-lg focus:bg-blue-900 focus:shadow-lg
-                focus:outline-none focus:ring-0 active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out py-2" 
-                onClick={openModal}>Registrar Filme
-            </button>
+    <>       
+        <div className="flex py-2 bg-blue-700 flex dark:bg-blue-700">  
+        
+          <button className="px-2 m-2 bg-gray-700 text-white text-sm
+            leading-tight uppercase rounded shadow-md hover:bg-gray-600 hover:shadow-lg focus:bg-blue-900 focus:shadow-lg
+            focus:outline-none focus:ring-0 active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out py-2" 
+            onClick={openModal}>Registrar Filme
+          </button>
+          
             <Modal
               style={customStyles}
               isOpen={modalIsOpen}          
               onRequestClose={closeModal}       
               contentLabel="Registrar novo filme"
             >            
-              <button onClick={closeModal}><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg></button>
-              <div className="w-full max-w-xs">
-                <form onSubmit={handleSubmitMovie} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" >
-                      Título
-                    </label>
-                    <input onChange={event => setTitle(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Título" required/>
-                  </div>  
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" >
-                      Descrição
-                    </label>
-                    <input onChange={event => setDescription(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Descrição" required/>
-                  </div> 
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" >
-                      Visualizações
-                    </label>
-                    <input onChange={event => setViews(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Visualizações" />
-                  </div>                   
-                  <button className="px-2 py-2 bg-blue-800 text-white text-sm
-                    leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-900 focus:shadow-lg
-                    focus:outline-none focus:ring-0 active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out" 
-                    type="submit"
-                      >Registrar Filme
-                  </button>              
-                </form>               
-              </div>
-            </Modal>
-                               
-            <nav className="relative z-0 inline-flex rounded-md shadow-sm flex space-x-2" aria-label="Pagination">
-              <button 
-                disabled={previousButton}              
-                onClick={() => handleWithPreviousPage()}
-                className="relative inline-flex items-center px-2 py-2 rounded-l-md border bg-white  
-                text-gray-500 hover:bg-blue-200"
-              >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </button>              
-              <a                  
-                aria-current="page"
-                className="z-10 bg-indigo-50 text-gray-800 relative inline-flex items-center px-4 py-2
-                border text-sm rounded"
-              >
-                {currentPage}
-              </a>                    
-              <button
-                disabled={nextButton}
-                onClick={() => handleWithNextPage()}
-                className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 
-                bg-white text-sm text-gray-500 hover:bg-blue-200"
-              >             
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </nav>
-          </div>
+            <button onClick={closeModal}><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg></button>
+            <div className="w-full max-w-xs">
+              <form onSubmit={handleSubmitMovie} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Título
+                  </label>
+                  <input onChange={event => setTitle(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Título" required/>
+                </div>  
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Descrição
+                  </label>
+                  <input onChange={event => setDescription(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Descrição" required/>
+                </div> 
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Visualizações
+                  </label>
+                  <input onChange={event => setViews(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Visualizações" />
+                </div>                   
+                <button className="px-2 py-2 bg-blue-800 text-white text-sm
+                  leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-900 focus:shadow-lg
+                  focus:outline-none focus:ring-0 active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out" 
+                  type="submit"
+                    >Registrar Filme
+                </button>              
+              </form>               
+            </div>
+          </Modal>            
+            
+          <nav className="relative z-0 inline-flex rounded-md shadow-sm flex space-x-2" aria-label="Pagination">
+            <button 
+              disabled={previousButton}              
+              onClick={() => handleWithPreviousPage()}
+              className="relative inline-flex items-center px-2 py-2 rounded-l-md border bg-white  
+              text-gray-500 hover:bg-blue-200"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>              
+            <a                  
+              aria-current="page"
+              className="z-10 bg-indigo-50 text-gray-800 relative inline-flex items-center px-4 py-2
+              border text-sm rounded"
+            >
+              {currentPage}
+            </a>                    
+            <button
+              disabled={nextButton}
+              onClick={() => handleWithNextPage()}
+              className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 
+              bg-white text-sm text-gray-500 hover:bg-blue-200"
+            >             
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </nav>          
         </div>
-      </div> 
+
 
       <table className="w-full text-sm text-center">               
         <thead className="text-sm text-gray-100 uppercase bg-blue-500">
-          <tr>                                
+          <tr> 
+            <th className="text-md">Ações</th>                                 
             <th className="text-md">Título</th>
             <th className="text-md">Descrição</th>
             <th className="text-md">Visualizações</th>
-            <th className="text-md">Data de Envio</th>   
-            <th className="text-md">Ações</th>           
+            <th className="text-md">Data de Envio</th> 
           </tr>
         </thead>
         <tbody>
           {movies.map((movie: Movie) => (                   
         <tr className="text-white border-b bg-gray-800 border-gray-700 odd:bg-white even:bg-gray-50 odd:bg-gray-800
-          even:bg-gray-700  md:transition-all ease-in-out delay-10 hover:bg-blue-500 duration-300" key={movie.id}>                         
+          even:bg-gray-700  md:transition-all ease-in-out delay-10 hover:bg-blue-500 duration-300" key={movie.id}>
+          <td className="px-6 py-4 border-slate-600 dark:border-slate-600"> 
+            <button                          
+              onClick={() => handleWithDelete(movie.id)}
+              className="relative inline-flex items-center px-2 py-2 rounded-l-md border bg-gray-300 hover:bg-gray-700  
+              text-gray-500 hover:bg-blue-200"
+              >             
+              <TrashIcon className="h-5 w-5" aria-hidden="true" />
+            </button>   
+          </td>                         
           <td className="px-6 py-4 border border-slate-600 dark:border-slate-600">
             {movie.title}
           </td>        
@@ -204,16 +212,7 @@ const customStyles = {
           <td className="px-6 py-4 border border-slate-600 dark:border-slate-600"> 
             {new Intl.DateTimeFormat("pt-BR", {}).format(
             new Date(movie.published))}
-          </td>  
-          <td className="px-6 py-4 border-slate-600 dark:border-slate-600"> 
-            <button                          
-              onClick={() => handleWithDelete(movie.id)}
-              className="relative inline-flex items-center px-2 py-2 rounded-l-md border bg-gray-300 hover:bg-gray-700  
-              text-gray-500 hover:bg-blue-200"
-              >             
-              <TrashIcon className="h-5 w-5" aria-hidden="true" />
-            </button>   
-          </td>                                           
+          </td> 
         </tr>                 
         ))}
         </tbody>                
